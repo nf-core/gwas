45fe49aafcbd0289ef99d4c5b36a4859febdc481
Davi Marcon (@mxrcon) <davijosuemarcon@gmail.com>
2022-08-08 22:59:18 -0300

    creating a template for arg parsing

--- templates/md5.py
+++ templates/md5.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 import hashlib
 from os.path import basename
-# Taken from http://stackoverflow.com/questions/3431825/generating-an-md5-checksum-of-a-file
+import argparse
 
+
+# argparsing
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--bed",action='store', help="Bed File",required = True)
+parser.add_argument("--bim",action='store', help="Bim File",required = True)
+parser.add_argument("--fam",action='store', help="Fam File",required = True)
+parser.add_argument("--out",action='store', help="Fam File",required = True)
+
+args = parser.parse_args()
+
+plinks = [args.bed,args.bim,args.fam]
+
+# Taken from http://stackoverflow.com/questions/3431825/generating-an-md5-checksum-of-a-file
 def md5sum(filename, blocksize=65536):
     hash = hashlib.md5()
     with open(filename, "rb") as f:
@@ -12,7 +26,6 @@ def md5sum(filename, blocksize=65536):
 
 
 md5s=[]
-plinks=[ "$bed","$bim","$fam"]
 
 for x in plinks:
     md5s.append((basename(x),md5sum(x)))
@@ -20,7 +33,7 @@ for x in plinks:
 
 
 
-f=open("$out","w")
+f=open(args.out,"w")
 for (fn,md) in md5s:
     f.write("%s\\t%s\\n"%(fn,md))
 f.close()
